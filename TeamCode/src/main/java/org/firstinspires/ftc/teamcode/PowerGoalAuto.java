79f040a724168e12a119185710852d7dbc8e8a6f
Avanti&Easha <thunderbotsteam@gmail.com>
2020-10-03 15:03:28 -0700

    avanti and easha created an autonomous mode for shooting at the power goal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerGoalAuto.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name="Power Goal Auto", group="Thunderbots")
+public class PowerGoalAuto extends MacThunderbotsSquareAutonomous {
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+        //initSkystoneCamera();
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Init done");    //
+        telemetry.update();
+
+        robot.leftDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                robot.leftDrive1.getCurrentPosition(),
+                robot.rightDrive1.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        gettotargetline();
+        strafelefttopowergoal();
+        moveforwardandpark();
+
+            // Step through each leg of the path
+
+    }
+
+    public void gettotargetline() {
+        //moveforwardtolaunchline
+
+        double powerMultiplier = 0.5;
+
+        robot.leftDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.leftDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        encoderDrive(DRIVE_SPEED, 24, 24, 1.8);
+
+    }
+
+    public void strafelefttopowergoal() {
+        //robot gets into position to shoot the power goals//
+
+        double powerMultiplier = 2;
+
+        robot.leftDrive1.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.rightDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.leftDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        encoderDrive(DRIVE_SPEED, 24, 24, 2.1);
+
+
+    }
+
+    public void moveforwardandpark() {
+
+        sleep(5000);
+        //we'll enter the code for the shooter to shoot before this (once it is built)//
+
+        double powerMultiplier = 0.5;
+
+        robot.leftDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.leftDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        encoderDrive(DRIVE_SPEED, 24, 24, 0.3);
+
+
+            }
+        }
+
