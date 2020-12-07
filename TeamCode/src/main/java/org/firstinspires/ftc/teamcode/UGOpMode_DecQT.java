db3d41cb89013bdc39be828b2d1b8b9539e554f8
avantiramraj <avantiramraj@gmail.com>
2020-12-06 20:01:12 -0800

    avanti - adding additional telemetry for collector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGOpMode_DecQT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGOpMode_DecQT.java
@@ -241,12 +241,12 @@ public class UGOpMode_DecQT extends LinearOpMode {
         telemetry.addData("Collector buttons status - lift ", liftCollector );
         telemetry.addData("Collector buttons status - go down", letGoCollector );
         if (liftCollector & !collectorUp) {
-            robot.collectorServo.setPosition(MAX_POS);
+            robot.collectorServo.setPosition(MIN_POS);
             telemetry.addData("Collector Lifted ", collectorPosition);
 
         }
         else if (letGoCollector & collectorUp ) {
-            robot.collectorServo.setPosition(MIN_POS);
+            robot.collectorServo.setPosition(MAX_POS);
             telemetry.addData("Collector Let Go ", collectorPosition);
         }
     }
