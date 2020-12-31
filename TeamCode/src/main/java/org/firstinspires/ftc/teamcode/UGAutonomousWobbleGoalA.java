6d660d6156540f40881100c374ffe6cd24436cba
thunderbotsftc <52214174+thunderbotsftc@users.noreply.github.com>
2020-12-30 17:41:39 -0800

    diya - removed getCurrentPosition of dc motors (rightdrive1 and leftdrive1) to get rid of null pointer exceptions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGAutonomousWobbleGoalA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGAutonomousWobbleGoalA.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.bak.ThunderbotsSquareAutonomous;
 
 @Autonomous(name="WobbleGoalA", group="Thunderbots")
 public class UGAutonomousWobbleGoalA extends UGTowerGoalBaseAuto {
     UGHardwarePushbot robot = new UGHardwarePushbot();
+    private ElapsedTime runtime = new ElapsedTime();
     double powerMultiplier = 0.5;
     double shooterPowerMultiplier = 0.5;
 
@@ -25,11 +27,13 @@ public class UGAutonomousWobbleGoalA extends UGTowerGoalBaseAuto {
         robot.rightDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.wobbleArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.shooterMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.wobbleArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
@@ -68,8 +72,8 @@ public class UGAutonomousWobbleGoalA extends UGTowerGoalBaseAuto {
         encoderDrive(DRIVE_SPEED, 5, 5, 0.3);
 
 
-        robot.leftDrive1.setDirection(DcMotorSimple.Direction.REVERSE);
-        robot.rightDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.leftDrive1.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive1.setDirection(DcMotorSimple.Direction.REVERSE);
         robot.leftDrive2.setDirection(DcMotorSimple.Direction.REVERSE);
         robot.rightDrive2.setDirection(DcMotorSimple.Direction.FORWARD);
 
@@ -90,11 +94,12 @@ public class UGAutonomousWobbleGoalA extends UGTowerGoalBaseAuto {
 
         //shoot 3 pre-loaded rings
         for (int a = 1; a <= 3; a++) {
+            sleep(1000);
             telemetry.addData("Status", "Trigger");
-            double triggerposition = this.robot.triggerServo.MIN_POSITION+1.5;
+            double triggerposition = this.robot.triggerServo.MAX_POSITION+1.5;
             robot.triggerServo.setPosition(triggerposition);
             telemetry.addData("Status", "FoundationArmUp");
-            triggerposition = this.robot.triggerServo.MAX_POSITION-1.5;
+            triggerposition = this.robot.triggerServo.MIN_POSITION-1.5;
             robot.triggerServo.setPosition(triggerposition);
 
         }
@@ -110,6 +115,77 @@ public class UGAutonomousWobbleGoalA extends UGTowerGoalBaseAuto {
 
         encoderDrive(DRIVE_SPEED, 24, 24, 0.8);
     }
+
+    public void encoderDrive(double speed,
+                               double leftInches, double rightInches,
+                               double timeoutS) {
+        int newLeftTarget1;
+        int newRightTarget1;
+        int newLeftTarget2;
+        int newRightTarget2;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget1 = robot.leftDrive1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget1 = robot.rightDrive1.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newLeftTarget2 = robot.leftDrive2.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget2 = robot.rightDrive2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+            robot.leftDrive1.setTargetPosition(newLeftTarget1);
+            robot.rightDrive1.setTargetPosition(newRightTarget1);
+            robot.leftDrive2.setTargetPosition(newLeftTarget2);
+            robot.rightDrive2.setTargetPosition(newRightTarget2);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftDrive1.setPower(Math.abs(speed));
+            robot.rightDrive1.setPower(Math.abs(speed));
+            robot.leftDrive2.setPower(Math.abs(speed));
+            robot.rightDrive2.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftDrive1.isBusy() && robot.rightDrive1.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget1, newRightTarget1);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftDrive1.getCurrentPosition(),
+                        robot.rightDrive1.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.leftDrive1.setPower(0);
+            robot.rightDrive1.setPower(0);
+            robot.leftDrive2.setPower(0);
+            robot.rightDrive2.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+
 }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGAutonomousWobbleGoalBaseC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGAutonomousWobbleGoalBaseC.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="WobbleGoalC", group="Thunderbots")
 public class UGAutonomousWobbleGoalBaseC extends UGTowerGoalBaseAuto {
     UGHardwarePushbot robot = new UGHardwarePushbot();
+    private ElapsedTime runtime = new ElapsedTime();
     @Override
     public void runOpMode() {
 
@@ -85,6 +87,75 @@ public class UGAutonomousWobbleGoalBaseC extends UGTowerGoalBaseAuto {
 
 
 
+    }
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget1;
+        int newRightTarget1;
+        int newLeftTarget2;
+        int newRightTarget2;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget1 = robot.leftDrive1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget1 = robot.rightDrive1.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newLeftTarget2 = robot.leftDrive2.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget2 = robot.rightDrive2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+            robot.leftDrive1.setTargetPosition(newLeftTarget1);
+            robot.rightDrive1.setTargetPosition(newRightTarget1);
+            robot.leftDrive2.setTargetPosition(newLeftTarget2);
+            robot.rightDrive2.setTargetPosition(newRightTarget2);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftDrive1.setPower(Math.abs(speed));
+            robot.rightDrive1.setPower(Math.abs(speed));
+            robot.leftDrive2.setPower(Math.abs(speed));
+            robot.rightDrive2.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftDrive1.isBusy() && robot.rightDrive1.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget1, newRightTarget1);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftDrive1.getCurrentPosition(),
+                        robot.rightDrive1.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.leftDrive1.setPower(0);
+            robot.rightDrive1.setPower(0);
+            robot.leftDrive2.setPower(0);
+            robot.rightDrive2.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            //  sleep(250);   // optional pause after each move
+        }
     }
 }
 
